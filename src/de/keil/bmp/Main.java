60ca5ca21e49a9c92b6de95ddd7596d0fe5cd2f0
Matthias Keil <keil@rm-keil.de>
2011-06-18 12:59:33 +0200

    initial commit
    Signed-off-by: Matthias Keil <keil@rm-keil.de>

---
+++ src/de/keil/bmp/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.keil.bmp;
+
+/**
+ * @author Matthias
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
