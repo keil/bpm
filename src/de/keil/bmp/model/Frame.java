02c1dfed62d45ef83445a228c46ca63a72d44078
Matthias Keil <keil@proglang-keil.fritz.box>
2011-10-11 08:57:51 +0200

    new frame

---
+++ src/de/keil/bmp/model/Frame.java
@@ -0,0 +1,18 @@
+package de.keil.bmp.model;
+
+import java.util.ArrayList;
+
+public class Frame {
+
+	private ArrayList<Sample> samples;
+	private int size;
+		
+	private Frame() {
+		
+	}
+	
+	
+	//get(4)
+	
+	
+}
