4a4c2d08215b0cbdad83f9b857599ee9474519b9
Matthias Keil <keil@proglang-keil.fritz.box>
2011-10-12 20:56:59 +0200

    layout

--- src/de/keil/bpm/basic/gui/MainFrame.java
+++ src/de/keil/bpm/basic/gui/MainFrame.java
@@ -3,9 +3,15 @@
  */
 package de.keil.bpm.basic.gui;
 
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -33,23 +39,42 @@ public class MainFrame extends Frame implements WindowListener, KeyListener {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Long> timestamps;
 	private long lastTimestamp;
-	
+
 	private ArrayList<Long> timeFrame;
 	
+	// FONTS
+	private Font fontLabel = new Font(Font.SANS_SERIF, Font.PLAIN, 8);
+	private Font fontValueNormal = new Font(Font.SANS_SERIF, Font.PLAIN, 28);
+	private Font fontValueBig = new Font(Font.SANS_SERIF, Font.PLAIN, 32);	
 	
-	private Label lMean;
-	private Label lMeanRound;
+	// COLORS
+	// TODO: Colors
 	
-	private Label lCurrent;
-	private Label lMeanUpper;
-	private Label lMeanLower;
+	// DEFAULT TEXT
+	private String defaultNumber = "00";
+	private String defaultDouble1 = "00.0";
+	private String defaultDouble2 = "00.00";
 	
+	// Button
+	private Button bClear;
+	private Button bStart;
+
+	// LABELS
+	private Label lMeanLabel;
+	private Label lMeanValue;
+	private Label lMeanLabelR;
+	private Label lMeanValueR;
 	
-	// meant tackt 45.6 
-	// meant tackt 45
+	private Label lCurrentLabel;
+	private Label lCurrentValue;
+	private Label lMeanUpperLabel;
+	private Label lMeanUpperValue;
+	private Label lMeanLowerLabel;
+	private Label lMeanLowerValue;
 	
-	// current tackt    lower
-	//                  upper
+	// TODO 10 messungs state
+	// TODO: Drop down Box
+	
 	
 	
 	
@@ -59,7 +84,10 @@ public class MainFrame extends Frame implements WindowListener, KeyListener {
 
 	public MainFrame() {
 		super("basic bpm");
-
+		
+		init();
+		clear();
+		
 		Dimension frameSize = new Dimension(400, 600);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
@@ -68,21 +96,174 @@ public class MainFrame extends Frame implements WindowListener, KeyListener {
 
 		setSize(frameSize);
 		setLocation(leftLocation, topLocation);
+
+		
+		
 		setVisible(true);
 
 		timestamps = new ArrayList<Long>();
 		timeFrame = new ArrayList<Long>();
 		lastTimestamp = 0;
 
-		init();
+		
 
 		addWindowListener(this);
 		addKeyListener(this);
 	}
 
 	private void init() {
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		setLayout(layout);
+		
+		// TOP PANEL
+		Panel topPanel = new Panel();
+		topPanel.setSize(new Dimension(500, 100));
+		topPanel.setBackground(Color.PINK); // TODO
+		topPanel.setLayout(new GridBagLayout());
+		
+		// LEFT PANEL
+		Panel leftPanel = new Panel();
+		leftPanel.setSize(300, 200);
+		leftPanel.setBackground(Color.DARK_GRAY); // TODO
+		leftPanel.setLayout(new GridBagLayout());
+		
+		// RIGHT PANEL
+		Panel rightPanel = new Panel();
+		rightPanel.setSize(200, 400);
+		rightPanel.setBackground(Color.BLUE); // TODO
+		rightPanel.setLayout(new GridBagLayout());
+		
+		
+		
+		//////////////////////////////////////////////////
+		// TOP
+		//////////////////////////////////////////////////
+		
+		bClear = new Button("clear");// TODO: action Listener
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		topPanel.add(bClear, c);
+
+
+		bStart = new Button("start");// TODO: how to run ?
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		topPanel.add(bStart, c);
+	
+		
+		
+		//////////////////////////////////////////////////
+		// LEFT
+		//////////////////////////////////////////////////
+		
+		lMeanLabel = new Label("Mean");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		leftPanel.add(lMeanLabel, c);
+		
+		lMeanValue = new Label(defaultDouble1);
+		lMeanValue.setFont(fontValueBig);
+		c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 1;
+	    c.gridy = 0;
+	    leftPanel.add(lMeanValue, c);
+	    
+	    lMeanLabelR = new Label("Mean");
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 0;
+	    c.gridy = 1;
+	    leftPanel.add(lMeanLabelR, c);
+
+	    lMeanValueR = new Label(defaultNumber);
+		lMeanValueR.setFont(fontValueNormal);
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    leftPanel.add(lMeanValueR, c);
+		
+		
+		
+		//////////////////////////////////////////////////
+		// RIGHT
+		//////////////////////////////////////////////////
+		
+	    lCurrentLabel = new Label("Current");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		rightPanel.add(lCurrentLabel, c);
+
+	    lCurrentValue = new Label(defaultDouble2);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		rightPanel.add(lCurrentValue, c);
+
+	    lMeanUpperLabel = new Label("Upper Variance");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		rightPanel.add(lMeanUpperLabel, c);
+
+		lMeanUpperValue = new Label(defaultDouble1);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 1;
+		rightPanel.add(lMeanUpperValue, c);
+
+	    lMeanLowerLabel = new Label("Lower Variance");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 2;
+		rightPanel.add(lMeanLowerLabel, c);
+
+		lMeanLowerValue = new Label(defaultDouble1);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 2;
+		rightPanel.add(lMeanLowerValue, c);
+
+		
+		
+		//////////////////////////////////////////////////
+		// BASE
+		//////////////////////////////////////////////////
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 100;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(topPanel, c);
+		
+		c.weightx = 300;
+		c.weighty = 200;
+	    c.gridx = 0;
+	    c.gridy = 1;
+	    add(leftPanel, c);
+	    
+	    c.weightx = 200;
+		c.weighty = 400;
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    add(rightPanel, c);
+
 	}
 
+	
+	private void clear() {
+		lMeanValue.setText(defaultDouble1);
+		lMeanValueR.setText(defaultNumber);
+		lCurrentValue.setText(defaultDouble2);
+		lMeanUpperValue.setText(defaultDouble1);
+		lMeanLowerValue.setText(defaultDouble1);
+	}
+	
+	
+	
 	private void add(long value) {
 		if(timeFrame.size() > 10)
 			timeFrame.remove(0);
