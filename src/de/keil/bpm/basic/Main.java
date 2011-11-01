3ea967c7e05ada236c0fa1b227db042928508f46
Matthias Keil <keil@rm-keil.de>
2011-11-01 22:00:04 +0100

    - splash images
    Signed-off-by: Matthias Keil <keil@rm-keil.de>

--- src/de/keil/bpm/basic/Main.java
+++ src/de/keil/bpm/basic/Main.java
@@ -1,10 +1,22 @@
 package de.keil.bpm.basic;
 
+import java.awt.SplashScreen;
+
 import de.keil.bpm.basic.gui.MainFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
+		final SplashScreen splash = SplashScreen.getSplashScreen();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		splash.close();
+
+		// Main Frame
 		new MainFrame();
 	}
 }
