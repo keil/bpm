d0e257caa61f5fae1622a8c5adc0b99bd5859168
Matthias Keil <keil@rm-keil.de>
2012-04-07 19:50:27 +0200

    - test eclipse swt as gui engine

--- src/de/keil/bpm/Test.java
+++ src/de/keil/bpm/Test.java
@@ -1,9 +1,41 @@
 package de.keil.bpm;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		Display display = new Display();
+	    final Shell shell = new Shell(display);
+	    shell.setSize(300, 300);
+	    shell.setLayout(new RowLayout());
+
+	    shell.setText("Composite Example");
+
+	    final Composite composite = new Composite(shell, SWT.NONE);
+	    GridLayout gridLayout = new GridLayout();
+	    gridLayout.numColumns = 4;
+	    composite.setLayout(gridLayout);
+
+	    for (int loopIndex = 0; loopIndex < 28; loopIndex++) {
+	      Label label = new Label(composite, SWT.SHADOW_NONE);
+	      label.setText("Label " + loopIndex);
+	    }
+
+	    shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch())
+	        display.sleep();
+	    }
+	    display.dispose();
+		
 		
 		
 //		float sampleRate = 8000;
